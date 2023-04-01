f64ea1f708a89775350158c3b28db005712f5e10
guicoelhodev <gs.coelho_dev@outlook.com>
2023-04-01 16:43:57 -0300

    feat: setup config files

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  innerWidth / innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(innerWidth, innerWidth);
+
+document.body.appendChild(renderer.domElement);
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+const mesh = new THREE.Mesh(boxGeometry, material);
+
+scene.add(mesh);
+
+camera.position.z = 5;
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+
+  // mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+};
+
+animate();
